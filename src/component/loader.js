87523ea066307a29fdfa0df3d69dc03d07e1b72c
Vishnu-IOT <93981819+Vishnu-IOT@users.noreply.github.com>
2026-02-05 18:15:23 +0530

    adding loader component

---
+++ src/component/loader.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../styles/hero.css';
+import loaderAnimation from '../lottie/loader.json';
+import Lottie from "react-lottie";
+
+function Loader() {
+
+    const loaderOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: loaderAnimation,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice",
+        },
+    };
+
+    return (
+        <div className="loader-container">
+            <Lottie
+                options={loaderOptions}
+                height={200}
+                width={200}
+            />
+            <p>Loading sarees...</p>
+        </div>
+    )
+}
+
+export default Loader
